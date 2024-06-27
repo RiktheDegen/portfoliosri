34beaf5775c610ad22d78ab7709419c1b426ef16
Srinath Hariharan <shacstudios@gmail.com>
2024-06-26 17:52:04 -0700

    id fornkets

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -80,7 +80,7 @@ If you think we should chat, Dm me on X or email me: srinathwork08@gmail.com
           <h2 className="text-xl font-bold mb-6">Mix of professional and personal projects</h2>
           <div className=" grid grid-cols-3 gap-3 tab:grid-cols-2  mob-l:grid-cols-1 mob-m:grid-cols-1  mob-s:grid-cols-1 ">
             {project.map((project) => (
-              <div key={project} className="bg-gray-200 p-4 rounded-lg">
+              <div key={project.id} className="bg-gray-200 p-4 rounded-lg">
                 <div className="w-full h-64 bg-gray-300 mb-4 rounded-lg"></div>
                 <h2>{project.Project}</h2>
                 <div className="flex flex-row gap-x-2 mt-1">
